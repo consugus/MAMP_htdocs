75d5b53358a02b770ee36f189b95c473591543a1
Gustavo <consugus@gmail.com>
2018-11-28 21:20:43 -0300

    Completado la recepción del pagos con PayPal, todo OK. Además se descargó la carpeta del modelo de recepción de pagos con PayPal del ejemplo terminado del curso

--- Proyecto Sitio de Conferencias/pago_finalizado.php
+++ Proyecto Sitio de Conferencias/pago_finalizado.php
@@ -1,25 +1,55 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/5.0.0/normalize.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-    <link rel="stylesheet" href="css/estilos.css">
-  </head>
-  <body>
-      <div class="formulario">
-            <h2>Pagos con Paypal</h2>
-            <?php
-                $resultado = (bool) $_GET['exito'];
-                $paymentId = $_GET['paymentId'];
+  <?php
+    include_once "includes/templates/header.php";
+    use PayPal\Rest\ApiContext;
+    use PayPal\Api\PaymentExecution;
+    use PayPal\Api\Payment;
+    require "includes/paypalConfig.php";
+  ?>
+
+    <section class="seccion contenedor">
+          <h2>Resumen Registro</h2>
+          <?php
+              $paymentId = $_GET['paymentId'];
+              $id_Pago = (int)$_GET['id_pago'];
+
+              // Petición a Rest API
+              $pago = Payment::get($paymentId, $apiContext); // En Pago le decimos con $paymentID cuál queremos revisar, iniciando sesión con nuestras credenciales de $ApiContext
+              $execution = new PaymentExecution(); // ejecutamos
+              $execution->setPayerId($_GET['PayerID']); //Indicamos qué pago queremos verificar si fué correcto
+
+              $resultado = $pago->execute($execution, $apiContext); // contiene la información de la transacción
+
+              $respuesta = $resultado->transactions[0]->related_resources[0]->sale->state;
+
+              // echo "<pre>";
+              //   var_dump($respuesta);
+              // echo "</pre>";
+
+              if($respuesta == 'completed') {
+                echo "<div class='resultado correcto'>";
+                echo    "El pago se realizo correctamente! </br> ";
+                echo    "El id es {$paymentId} ";
+                echo "</div>";
+                require_once('includes/funciones/dbconnection.php');
+                $stmt=$conn->prepare("UPDATE registrados SET pagado = ? WHERE id_registrado = ?");
+                $pagado = 1;
+                $stmt->bind_param('ii', $pagado, $id_Pago);
+                $stmt->execute();
+                $stmt->close();
+                $conn->close();
+
+              } else {
+                echo "<div class='resultado error'>";
+                echo "El pago no se realizó";
+                echo "</div>";
 
 
-                if($resultado == true) {
-                      echo "El pago se realizo correctamente! </br> ";
-                      echo "El id es {$paymentId} ";
-                }
-             ?>
-        </div>
-  </body>
-</html>
+
+              }
+            ?>
+      </div>
+
+
+  <?php include_once "includes/templates/footer.php" ?>
+
+</section>
--- Proyecto Sitio de Conferencias/validar_registro.php
+++ Proyecto Sitio de Conferencias/validar_registro.php
@@ -13,8 +13,6 @@
                 include_once "includes/funciones/funciones.php";
                 $pedido = productos_json($boletos, $camisas, $etiquetas);
 
-                //echo "<pre>";  var_dump($pedido);  echo "<pre>";
-
                 // Eventos
                 $eventos = $_POST['registro'];
                 $registro = eventos_json($eventos);
@@ -31,14 +29,14 @@
                     $query .= "total_pagado )";
                     $query .= "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
                     $stmt = $conn->prepare($query);
-                    // Se coloca una legra s por cada parámetro que se ingresa que no sea un entero, y una letra
-                    // i por cada valor númerico
+                    // Se coloca una letra s por cada parámetro alfabético que se ingresa (string), y una letra
+                    // i por cada valor númerico (integer)
                     $stmt ->bind_param("ssssssis", $nombre, $apellido, $email, $fecha, $pedido, $registro, $regalo, $total);
                     $stmt->execute();
                     $stmt->close();
                     $conn->close();
                     header("Location: validar_registro.php?Exitoso=1");
-                } catch(\Exception $e){
+                } catch(Exception $e){
                     echo $e->getMessage();
                 }
             ?>
