874c6b80628d3da0819646fb4d1b8aae5bb0c02e
Gustavo <consugus@gmail.com>
2019-01-10 20:21:08 -0300

    Lectura a servidor externo

--- Proyecto Sitio de Conferencias/includes/funciones/dbconnection.php
+++ Proyecto Sitio de Conferencias/includes/funciones/dbconnection.php
@@ -1,5 +1,16 @@
 <?php
-    $conn = new mysqli('localhost', 'root', 'root', 'gldwebcamp');
+    $host = 'localhost';
+    $usuario_DB = 'root';
+    $password_DB = 'root';
+    $database = 'gldwebcamp';
+
+    // $host = '159.89.159.137:3306';
+    // $usuario_DB = 'user-staging';
+    // $password_DB = 'qmRH40pQgtLMarP0';
+    // $database = 'camp_consugus';
+
+
+    $conn = new mysqli($host, $usuario_DB, $password_DB, $database);
 
     if($conn->connect_error){
         echo $error->$conn->connect_error;
